d8ab83bb7b52aaa437df26097c59ce256fa01b65
sritthh <g3sritth@gmail.com>
2023-12-25 05:01:21 +0530

    hello

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -5,4 +5,4 @@ NUMBER=$2
 
 SUM=$(($NUMBER1+$NUMBER2))
 
-echo "Total::$sum"
+echo "Total:: $SUM"
---
+++ 07-arrays.sh
@@ -0,0 +1,7 @@
+
+
+FRUITS=("Apple" "Banana" "Mango")
+
+echo "First value: ${FRUITS[0]}"
+
+echo  "second value: ${FRUITS[1]}"
