7c24ec1f4cba241a243bb20efe82b865c4c10fca
sritthh <g3sritth@gmail.com>
2023-12-24 12:48:23 +0530

    hello

---
+++ 02-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
+
+
+echo "$PERSON1 : Hello  Good $PERSON2 Morning"
+echo "$PERSON2 : Hi $PERSON1 Very Good Morning"
+echo "$PERSON1 : How are you $PERSON2"
+echo "$PERSON2  : Iam good $PERSON1,How are you"
+
+
