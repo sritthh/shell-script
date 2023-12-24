05d5370eb3be0e133d81553b23d67fca1655086b
sritthh <g3sritth@gmail.com>
2023-12-25 04:08:43 +0530

    hello

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,7 +2,7 @@
 
 echo "please enter your name"
 
-read USERNAME #the value entered above will be automatically attached to USERNAME variable
+read -s USERNAME #the value entered above will be automatically attached to USERNAME variable
 
 echo "username is: $USERNAME"
 
