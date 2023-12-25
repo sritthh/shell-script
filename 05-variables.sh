c232dbb7a5f870360d9a53d25b59e0eaf9150388
sritthh <g3sritth@gmail.com>
2023-12-25 16:27:28 +0530

    hello

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,13 +1,13 @@
 #!/bash/bin
 
-echo "please enter your name::"
+echo "please enter your username::"
 
 read -s USERNAME #the value entered above will be automatically attached to USERNAME variable
 
 echo "username is: $USERNAME" # Iam printing just for validation,you should not print
-username and passwords in scripts
+username and password in scripts
 
-echo "please enter your passwords::"
+echo "please enter your password::"
 
 echo "username is : $USERNAME,password is : $PASSWORD,you should not print user and password
 in scripts
