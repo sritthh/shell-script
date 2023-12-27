31b045d5b2769403bcd3505488c5cd09e440c2dc
sritthh <g3sritth@gmail.com>
2023-12-27 14:21:03 +0530

    hello

---
+++ 09-install-mysql.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then 
+    echo "ERROR:: please run this script with root access"
+    exit 1 # you can give other than 0
+else
+    echo "you are root user"
+fi # fi means reverse of fi, indicating condition end
