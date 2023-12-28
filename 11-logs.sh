73e3d6d7eb97f0a166debe037374340cabc2ea41
sritthh <g3sritth@gmail.com>
2023-12-28 14:48:49 +0530

    hello

---
+++ 11-logs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+echo "script name: $0"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "ERROR:: $2 ..... failed"
+        exit1
+    else
+        echo " $2 .... success"
+     fi   
+}
+
+if [ $ID -ne 0 ]
+then 
+    echo "ERROR:: please run this script with root access"
+    exit 1 # you can give other than 0
+else
+    echo "you are root user"
+fi
+
+VALIDATE $? "installing MYSQL" 
+
+yum install git -y
+
+VALIDATE $? "Installing GIT"
