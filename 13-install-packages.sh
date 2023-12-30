4ff9049fc03851fda16a56f73c1da274064ec9fd
sritthh <g3sritth@gmail.com>
2023-12-30 17:02:18 +0530

    hello

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -1,5 +1,36 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+R="\e[31m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started excuting $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
 
 
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ...$R failed $N"
+        exit 1
+    else
+        echo -e " $2 ...$G success $N"
+     fi 
+}
+
+if [ $ID -ne 0 ]
+then 
+    echo -e "$R ERROR:: please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "you are root user"
+fi
 # echo "All arguments that passed: $@"
 # git mysql postfix net tools
 # package=git for first time
@@ -14,33 +45,8 @@ do
      else
            echo -e "$package is already installed ...$Y SKIPPING $N"
      fi
-done               #!bin/bash
+done              
 
-ID=$(id -u)
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-TIMESTAMP=$(date +%F-%H-%M-%S)
-LOGFILE="/tmp/$0-$TIMESTAMP.log"
-
-echo "script started excuting $TIMESTAMP" &>> $LOGFILE
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 .....$R failed $N"
-        exit 1
-    else
-        echo -e " $2 ....$G success $N"
-     fi 
-
-if [ $ID -ne 0 ]
-then 
-    echo -e "$R ERROR:: please run this script with root access $N"
-    exit 1 # you can give other than 0
-else
-    echo "you are root user"
-fi
+    
+   
