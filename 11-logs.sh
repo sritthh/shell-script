2f9c8d91f087bd326c4b366f46b75eb04e283617
sritthh <g3sritth@gmail.com>
2023-12-30 13:11:16 +0530

    hello

--- 11-logs.sh
+++ 11-logs.sh
@@ -23,7 +23,7 @@ VALIDATE(){
 
 if [ $ID -ne 0 ]
 then 
-    echo "$R ERROR:: please run this script with root access $N"
+    echo -e "$R ERROR:: please run this script with root access $N"
     exit 1 # you can give other than 0
 else
     echo "you are root user"
